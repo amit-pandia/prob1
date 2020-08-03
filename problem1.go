0a03d56500a2fa1f09a53469594a45f85082c62e
amit-pandia <amit.pandia@calsoftinc.com>
2020-08-03 08:48:23 +0530

    improved logic

--- problem1.go
+++ problem1.go
@@ -9,17 +9,22 @@ type testWrapper struct {
 	testData  []test
 }
 
+type roseDetails struct {
+	roseCount int
+	idx       int
+}
+
 type test struct {
 	shopCount    int
-	rosesPerShop []int
+	rosesPerShop []roseDetails
 	result       int
+	ignoredShops map[int]int
 }
 
 func main() {
 
+	// get inputs
 	tw := testWrapper{}
-	// read no. of tests
-	// fmt.Println("Enter Test cases:")
 	_, err := fmt.Scanf("%d", &tw.TestCount)
 	if err != nil {
 		return
@@ -28,47 +33,56 @@ func main() {
 	for i := 1; i <= tw.TestCount; i++ {
 		t := test{}
 
-		// read no. of shops
-		// fmt.Println("Enter no of shops:")
+		// get no of test cases
 		_, err := fmt.Scanf("%d", &t.shopCount)
 		if err != nil {
 			return
 		}
 
-		length := t.shopCount
-		//fmt.Println("Enter the inputs: ")
-		t.rosesPerShop = make([]int, length)
-		for i := 0; i < length; i++ {
-			fmt.Scan(&t.rosesPerShop[i])
-		}
-
-		// calculate result
-		oddSum := 0
-		evenSum := 0
-
+		// read test inputs
+		t.rosesPerShop = make([]roseDetails, t.shopCount)
 		for i := 0; i < t.shopCount; i++ {
-			if (i % 2) == 0 {
-				evenSum += t.rosesPerShop[i] // sum of roses provided by even numbered shops
-			} else {
-				oddSum += t.rosesPerShop[i] // sum of roses provided by odd numbered shops
-			}
-		}
-
-		if oddSum > evenSum {
-			t.result = oddSum
-		} else if evenSum > oddSum {
-			t.result = evenSum
-		} else {
-			t.result = evenSum
+			t.rosesPerShop[i].idx = i
+			fmt.Scan(&t.rosesPerShop[i].roseCount)
+			t.ignoredShops = map[int]int{}
 		}
 
 		tw.testData = append(tw.testData, t)
+	}
+
+	// Bubble sort: sort in decending order of rose count
+	for _, t := range tw.testData {
+		for i := 0; i < len(t.rosesPerShop); i++ {
+			for j := i + 1; j < len(t.rosesPerShop); j++ {
+				if t.rosesPerShop[i].roseCount < t.rosesPerShop[j].roseCount {
+					tmp := t.rosesPerShop[j]
+					t.rosesPerShop[j] = t.rosesPerShop[i]
+					t.rosesPerShop[i] = tmp
+				}
+			}
+		}
+	}
+
+	// calculate result
+	for tIdx, t := range tw.testData {
+		for _, r := range t.rosesPerShop {
+			_, ok := t.ignoredShops[r.idx]
+			if !ok {
+				t.result += r.roseCount
+			}
+			if r.idx < len(t.rosesPerShop)-1 {
+				t.ignoredShops[r.idx+1] = r.idx + 1
+			}
+			if r.idx > 0 {
+				t.ignoredShops[r.idx-1] = r.idx - 1
+			}
+		}
+		tw.testData[tIdx] = t
 	}
 
 	fmt.Println("Output:")
 	for _, t := range tw.testData {
 		fmt.Println(t.result)
 	}
-
 	return
 }
